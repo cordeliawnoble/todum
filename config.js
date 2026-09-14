2ac3d4f5133817d606d041b1d568cd833ed42e7e
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 17:34:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.TODUM_CONFIG = {
   // À renseigner quand on branche l'apparence à ton projet Supabase existant.
   supabaseUrl: "https://uvlzrfxecmaugdfuhjho.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2bHpyZnhlY21hdWdkZnVoamhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1OTA5NjMsImV4cCI6MjEwNDE2Njk2M30.RXYM5ZDpFcf_mFoon9UglTyXe_Iqoy9ljQUNu2cPr-s",
-  mediaBucket: "site-images",
+  mediaBucket: "Todum",
 
   // URL de ton futur hub Eternal Sonata.
   eternalSonataUrl: "#",
